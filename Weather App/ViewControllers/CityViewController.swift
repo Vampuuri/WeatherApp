9530f07068146110fb99ff3c5a22fae2fc8e8187
Vampuuri <essi.supponen@tuni.fi>
2019-10-09 14:59:26 +0300

    CityViewController: add file with initial code

---
+++ Weather App/ViewControllers/CityViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CityViewController.swift
+//  Weather App
+//
+//  Created by Essi Supponen on 09/10/2019.
+//  Copyright © 2019 Essi Supponen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CityViewController: UIViewController {
+    
+}
