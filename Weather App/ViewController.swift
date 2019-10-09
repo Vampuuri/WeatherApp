4883d1338ce4d26024b27717b81590478767b138
Essi Supponen <c7esuppo@kc408mac18.tamk.ad>
2019-10-09 13:36:19 +0300

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Essi Supponen on 09/10/2019.
+//  Copyright © 2019 Essi Supponen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
