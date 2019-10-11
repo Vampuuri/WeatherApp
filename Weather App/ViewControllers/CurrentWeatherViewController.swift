dd950ef1b1aa7b66a9c652064e1d8ec39b13563e
Vampuuri <essi.supponen@tuni.fi>
2019-10-11 16:14:45 +0300

    CurrentWeatherViewController: add updateWeather-method, that updates all information on the view

--- Weather App/ViewControllers/CurrentWeatherViewController.swift
+++ Weather App/ViewControllers/CurrentWeatherViewController.swift
@@ -14,12 +14,38 @@ class CurrentWeatherViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var iconImageView: UIImageView!
     
+    var weather: WeatherObject?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NSLog("Current Weather")
         
-        self.cityLabel.text = "Tampere"
-        self.temperatureLabel.text = "0 \u{00B0}C"
-        self.iconImageView.image = UIImage(named: "clear")
+        self.updateWeather(WeatherObject(city: "Tampere", temperature: 10.0, weatherType: "Rain"))
+    }
+    
+    func updateWeather(_ weather: WeatherObject) {
+        self.weather = weather
+        self.cityLabel.text = weather.city
+        self.temperatureLabel.text = "\(weather.temperature) \u{00B0}C"
+        
+        var image: UIImage?
+        
+        if weather.weatherType == WeatherType.clear {
+            image = UIImage(named: "clear")
+        } else if weather.weatherType == WeatherType.cloudy {
+            image = UIImage(named: "cloudy")
+        } else if weather.weatherType == WeatherType.partlyCloudy {
+            image = UIImage(named: "partlyCloudy")
+        } else if weather.weatherType == WeatherType.rainy {
+            image = UIImage(named: "rainy")
+        } else if weather.weatherType == WeatherType.snow {
+            image = UIImage(named: "snow")
+        } else if weather.weatherType == WeatherType.thunder {
+            image = UIImage(named: "thunder")
+        } else {
+            image = UIImage(named: "misty")
+        }
+        
+        self.iconImageView.image = image
     }
 }
