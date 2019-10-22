1c87c4365bbc903fcc0d995265922968767f9437
Vampuuri <essi.supponen@tuni.fi>
2019-10-22 12:46:50 +0300

    AppDelegate: add getPathToFile-method without code

--- Weather App/AppDelegate.swift
+++ Weather App/AppDelegate.swift
@@ -99,6 +99,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func getPathToFile(_ filename: String) -> String {
+        return ""
+    }
 
 }
 
