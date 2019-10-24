1e66150687feef5b6627cc273e88d8ef02380d1c
Vampuuri <essi.supponen@tuni.fi>
2019-10-24 12:36:21 +0300

    WeatherFecther: add basic fetchUrl method

--- Weather App/WeatherFetcher.swift
+++ Weather App/WeatherFetcher.swift
@@ -11,6 +11,26 @@ import Foundation
 class WeatherFetcher {
     private let APIKEY = "60881c23fa92d269a2479d5378c082d7"
     
+    func fetchUrl(url : String, completionBlock: @escaping (Optional<[String : Any]>) -> Void) {
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let url : URL? = URL(string: url)
+        
+        let task = session.dataTask(with: url!) {
+            (data: Data?, response: URLResponse?, error: Error?) in
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String : Any]
+                completionBlock(json)
+            } catch let error {
+                print(error.localizedDescription)
+                completionBlock(nil)
+            }
+        }
+        
+        task.resume();
+    }
+    
     static func fetchCurrentWeather(city: String) {
         
     }
