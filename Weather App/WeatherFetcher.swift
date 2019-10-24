bea986a84e47befd59bf695d4085bbb7da950799
Vampuuri <essi.supponen@tuni.fi>
2019-10-24 12:24:07 +0300

    WeatherFetcher: add methods for for fetching from both by city or by coordinates

--- Weather App/WeatherFetcher.swift
+++ Weather App/WeatherFetcher.swift
@@ -11,11 +11,19 @@ import Foundation
 class WeatherFetcher {
     private let APIKEY = "60881c23fa92d269a2479d5378c082d7"
     
-    static func fetchCurrentWeather() {
+    static func fetchCurrentWeather(city: String) {
         
     }
     
-    static func fetchWeatherForecast() {
+    static func fetchCurrentWeather(longitude: Double, latitude: Double) {
+        // Will be implemented later
+    }
+    
+    static func fetchWeatherForecast(city: String) {
         
+    }
+    
+    static func fetchWeatherForecast(longitude: Double, latitude: Double) {
+        // Will be implemented later
     }
 }
