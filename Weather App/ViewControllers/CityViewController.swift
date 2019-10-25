4b1a82b1e25af3bda3eba73f4c6801bf8a7ecd0a
Vampuuri <essi.supponen@tuni.fi>
2019-10-25 10:15:35 +0300

    CityView: make slight change to check problem with version control

--- Weather App/ViewControllers/CityViewController.swift
+++ Weather App/ViewControllers/CityViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 class CityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
-    var cities = ["Helsinki", "Tampere", "Loimaa"]
+    var cities = ["Use current location", "Helsinki", "Tampere", "Loimaa"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
